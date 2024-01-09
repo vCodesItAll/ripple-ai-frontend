52080b6d3e677f89e72e7b07be5b585c3be38dfa
tphill <tphill760@gmail.com>
2024-01-09 16:52:26 -0500

    plz

--- app/components/card.js
+++ app/components/card.js
@@ -3,9 +3,9 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Footer from "./footer";
-import crystals from "./images/crystals1.jpg";
-import butterfly from "./images/butterfly.png";
-import berry from "./images/berry1.jpg";
+import crystals from "../images/crystals1.jpg";
+import butterfly from "../images/butterflySketch.png";
+import berry from "../images/berry1.jpg";
 
 function Card() {
   return (
