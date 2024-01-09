a85435ad8789fb52865ffc8a42022f275a56978a
tphill <tphill760@gmail.com>
2024-01-09 15:31:36 -0500

    changed logo to import img

--- app/components/ripple-logo.js
+++ app/components/ripple-logo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import datLogo from "../images/ripple3.png"
 
 function Logo() {
   return (
     <div>
       <Image
-        src="/images/ripple3.png"
+        src={datLogo}
         alt="ripple image"
         height={400}
         width={600}
