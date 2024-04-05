ab262df678e5769e5ea8a48d22585138695713a8
tphill <tphill760@gmail.com>
2024-04-05 00:51:09 -0400

    new save

--- app/stories/page.js
+++ app/stories/page.js
@@ -1,18 +1,15 @@
-'use client'
+"use client";
 import React from "react";
 import Navbar from "../components/navbar";
 import Card from "../components/card";
-import ParticlesContainer from "../components/particles";
-
 
 function Stories() {
   return (
     <>
-    <ParticlesContainer />
-    <div className="h-screen">
-      <Navbar />
-      <Card />
-    </div>
+      <div className="h-screen">
+        <Navbar />
+        <Card />
+      </div>
     </>
   );
 }
